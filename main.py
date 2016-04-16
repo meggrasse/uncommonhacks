b25b6fed237a9bc0961a5a1401c284418e7fefab
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-04-16 14:37:00 -0500

    added tuples function

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+from app.sentiment import *
+
+def main():
+	if len(sys.argv) != 2:
+		print "usage: ./main.py {{string}}"
+		sys.exit(1)
+
+	user_str = sys.argv[1]
+
+	# data = get_ratings(user_str)
+	# pos = data['probability']['pos']
+	# neg = data['probability']['neg']
+	# neutral = data['probability']['neutral']
+
+	print get_sent_tuples(parse_string(user_str))
+
+if __name__ == '__main__':
+  main()
+
