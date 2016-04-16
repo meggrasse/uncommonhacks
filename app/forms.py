1a2aec5ef4b1f7e12791a1593820fd50b4fd0bd5
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-04-16 12:43:58 -0500

    edited readme

--- app/forms.py
+++ app/forms.py
@@ -3,4 +3,4 @@ from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class StringForm(Form): 
-	textinput = StringField('textinput', validators=[DataRequired()])
+	textinput = StringField('textinput', validators=[DataRequired()], render_kw={"placeholder": "Write some text here"})
---
+++ readme.md
@@ -0,0 +1,10 @@
+# uncommon hacks
+
+
+#### Installation
+- Clone this repository ```git clone git@github.com:meggrasse/uncommonhacks.git```
+
+#### Usage 
+- In your terminal, cd to the cloned repo
+- Run `python run.py`
+- Visit `localhost:6969s` to view the application in your browser
