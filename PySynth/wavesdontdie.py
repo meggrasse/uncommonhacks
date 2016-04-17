2b8ec42bda41fb03fa6b81052f5186ca01eab265
avaid96 <avaid1996@gmail.com>
2016-04-16 20:56:40 -0500

    sys ars for wavesdontdie, changes to 3 notes

--- PySynth/wavesdontdie.py
+++ PySynth/wavesdontdie.py
@@ -1,15 +1,21 @@
 import subprocess
 import shutil
 import os
+import sys
 
-commanda="python2 read_abc.py 4chord.abc.txt 1 --syn_b"
+file=""
+if sys.argv[1]==None:
+	print "Enter an abc file name"
+else: 
+	print "Working with your file: "+sys.argv[1]
+	file=sys.argv[1]
+
+commanda="python2 read_abc.py "+file+" 1 --syn_b"
 # ren1="ren out.wav 1.wav"
-commandb="python2 read_abc.py 4chord.abc.txt 2 --syn_b"
+commandb="python2 read_abc.py "+file+" 2 --syn_b"
 # ren2="ren out.wav 2.wav"
-commandc="python2 read_abc.py 4chord.abc.txt 3 --syn_b"
+commandc="python2 read_abc.py "+file+" 3 --syn_b"
 # ren3="ren out.wav 3.wav"
-commandd="python2 read_abc.py 4chord.abc.txt 4 --syn_b"
-# ren4="ren out.wav 4.wav"
 
 process = subprocess.Popen(commanda.split(), stdout=subprocess.PIPE)
 print process.communicate()[0]
@@ -23,18 +29,11 @@ process = subprocess.Popen(commandc.split(), stdout=subprocess.PIPE)
 print process.communicate()[0]
 # process = subprocess.Popen(ren3.split(), stdout=subprocess.PIPE)
 shutil.move('out.wav','3.wav')
-process = subprocess.Popen(commandd.split(), stdout=subprocess.PIPE)
-print process.communicate()[0]
-# process = subprocess.Popen(ren4.split(), stdout=subprocess.PIPE)
-shutil.move('out.wav','4.wav')
 
 merge12="python2 mixfiles.py 1.wav 2.wav first.wav"
-merge34="python2 mixfiles.py 3.wav 4.wav second.wav"
-final="python2 mixfiles.py first.wav second.wav final.wav"
+merge123="python2 mixfiles.py first.wav 3.wav final.wav"
 
 process = subprocess.Popen(merge12.split(), stdout=subprocess.PIPE)
 print process.communicate()[0]
-process = subprocess.Popen(merge34.split(), stdout=subprocess.PIPE)
-print process.communicate()[0]
-process = subprocess.Popen(final.split(), stdout=subprocess.PIPE)
+process = subprocess.Popen(merge123.split(), stdout=subprocess.PIPE)
 print process.communicate()[0]
